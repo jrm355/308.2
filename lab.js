7ff79dbd52c3a5eb691d1b8c345cc072c50eaac5
jrm355 <jeremykazdan34@gmail.com>
2024-09-10 20:20:47 -0400

    I tried

---
+++ lab.js
@@ -0,0 +1,83 @@
+// part 1
+// Your task is to create a program that advises a group of environmental 
+//scientists how to handle the growth and spread of a unique plant species under their supervision. 
+//You must develop a growth control system that will monitor and predict the plant growth, 
+//making decisions based on the available space and potential growth.
+
+const PI = 3.1415;
+const area = PI * radius * radius;
+
+//weeks post planting
+
+// area covered in plants
+let a = plants
+
+// The area in which the plants are contained is circular, with a radius of 5 meters
+
+PI * 5 * 5 
+//total radius
+// = 78.5375
+
+// Each plant requires a minimum space of 0.8 square meters.
+const space = .8
+
+
+//The area is starting with 20 plants.
+
+const start = 20
+
+//The plants double in number every week
+
+
+//Predict the plant growth after a specific number of weeks.
+
+let W = 1
+
+(.8 * 20 * 2 ** W) = a
+console.log (a)
+// = area
+
+// .8 is tree radius, 20 trees, growth rate of 2**2
+
+//w is weeks planted
+
+
+// Implement control flow to make decisions on whether the plants should be:
+// This condition should be met if the plant count after the given number of weeks is greater than 80% of the maximum capacity of the garden.
+
+if (78.5375 > .8 * a) {
+    console.log ('pruned')
+    
+} else if (78.5375 <= .8 * a && 78.5375 >= .5 * a )
+     {console.log ('monitored')
+    
+}  else { console.log ('planted')}
+
+
+//week 1 logged as planted
+//week 2 logged as pruned
+//weed 3 logged as pruned
+
+
+// Part 2
+
+(.8 * 100 * 2**10) = area
+console.log (area)
+
+// solve for radius
+
+area = PI * radius * radius
+console.log (radius) 
+ 
+
+// part 3
+//try {
+  //  if (a < 78.5375) { console.log ('fine')
+        
+ //   } else {
+ //       throw "Error-please stop planting " 
+        
+   // }
+//} catch (error) {console.log (error)
+
+//}
